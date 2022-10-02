c8427a06a73084264f490df3781c28dd1ee02fa2
irinamarzioni <imarzioni@devartis.com>
2022-10-02 18:59:08 -0300

    correccion

--- libro.ts
+++ libro.ts
@@ -1,5 +1,4 @@
 class Libro {
-
     private nombre: string;
     private autor: string;
     private editorial: string;
@@ -14,106 +13,97 @@ class Libro {
         this.genero = pGenero;
     }
 
-public getNombre():string{
-    return this.nombre;
-}
+    public getNombre(): string {
+        return this.nombre;
+    }
 
-public getAutor():string{
-    return this.autor;
-}
+    public getAutor(): string {
+        return this.autor;
+    }
 
-public getEditorial():string{
-    return this.editorial;
-}
+    public getEditorial(): string {
+        return this.editorial;
+    }
 
-public getPrecio():void{
-    this.precio;
-}
+    public getPrecio(): void {
+        this.precio;
+    }
 
-public getGenero():string{
-    return this.genero;
-}
+    public getGenero(): string {
+        return this.genero;
+    }
 
-public setNombre(pNombre:string):void{
-this.nombre = pNombre;
-}
+    public setNombre(pNombre: string): void {
+        this.nombre = pNombre;
+    }
 
-public setAutor(pAutor:string):void{
-    this.autor = pAutor;
-}
+    public setAutor(pAutor: string): void {
+        this.autor = pAutor;
+    }
 
-public setEditorial(pEditorial:string):void{
-    this.editorial = pEditorial;
-}
+    public setEditorial(pEditorial: string): void {
+        this.editorial = pEditorial;
+    }
 
-public setPrecio(pPrecio:number):void{
-    this.precio = pPrecio;
-}
+    public setPrecio(pPrecio: number): void {
+        this.precio = pPrecio;
+    }
 
-public setGenero(pGenero:string):void{
-    this.genero = pGenero;
-}
-
+    public setGenero(pGenero: string): void {
+        this.genero = pGenero;
+    }
 }
 
 
 
 //#####################################################################################################//
 
-class GestorDeLibros{
+class GestorDeLibros {
+    private stockLibros: Libro[];
 
-private stockLibros:Libro[];
+    constructor(pStock: Libro[]) {
+        this.stockLibros = pStock;
+    }
 
-constructor(pStock:Libro[]){
-    this.stockLibros = pStock;
-}
+    public ingresarLibro(pNombre: Libro): void {
+        this.stockLibros.push(pNombre);
+        console.log("Se agrego el siguiente libro: " + pNombre.getNombre());
+    }
 
-public ingresarLibro(pNombre:Libro):void{
-    this.stockLibros.push(pNombre);
-    console.log("Se agrego el siguiente libro: " + pNombre.getNombre());
-}
-
-public eliminarLibro(pNombre:Libro):void{
-    for (let i = 0; i < this.stockLibros.length; i++){
-        if (pNombre.getNombre() === this.stockLibros[i].getNombre()){
-            this.stockLibros.splice(i, 1, stockLibros[i]);
-            console.log("Se elimino el siguiente libro: " + pNombre.getNombre());
-        } else {
-console.log("No se encontro el libro");
+    public eliminarLibro(pNombre: Libro): void {
+        for (let i = 0; i < this.stockLibros.length; i++) {
+            if (pNombre.getNombre() === this.stockLibros[i].getNombre()) {
+                this.stockLibros.splice(i, 1, stockLibros[i]);
+                console.log("Se elimino el siguiente libro: " + pNombre.getNombre());
+            } else {
+                console.log("No se encontro el libro");
+            }
         }
-    } 
-}
+    }
 
-public modificarLibro(pNombre:string,pAutor:string,pEditorial:string,pPrecio:number,pGenero:string):void{
-    for (let i = 0; i < this.stockLibros.length; i++){
-        if (pNombre === this.stockLibros[i].getNombre()){
+    public modificarLibro(pNombre: string, pAutor: string, pEditorial: string, pPrecio: number, pGenero: string): void {
+        for (let i = 0; i < this.stockLibros.length; i++) {
+            if (pNombre === this.stockLibros[i].getNombre()) {
 
-            this.stockLibros[i].setNombre(pNombre);
-            this.stockLibros[i].setAutor(pAutor);
-            this.stockLibros[i].setEditorial(pEditorial);
-            this.stockLibros[i].setPrecio(pPrecio);
-            this.stockLibros[i].setGenero(pGenero);
+                this.stockLibros[i].setNombre(pNombre);
+                this.stockLibros[i].setAutor(pAutor);
+                this.stockLibros[i].setEditorial(pEditorial);
+                this.stockLibros[i].setPrecio(pPrecio);
+                this.stockLibros[i].setGenero(pGenero);
 
-            console.log("Se modifico el siguiente libro: " + pNombre);
-        } else {
-            console.log("No se encontro el libro a modificar");
+                console.log("Se modifico el siguiente libro: " + pNombre);
+            } else {
+                console.log("No se encontro el libro a modificar");
+            }
         }
     }
 }
 
+let libro1 = new Libro("Relato de un naufrago", "Gabriel Garcia Marquez", "Debolsillo", 3000, "Ficcion");
+let libro2 = new Libro("Alicia en el pais de la maravillas", "Carroll Lewis", "Terramar", 1000, "Lituratura juvenil");
+let libro3 = new Libro("El principito", "Antoine De Saint Exupery", "Ethos editorial", 2000, "Infantil");
 
-
-
-
-
-
-}
-
-let libro1 = new Libro("Relato de un naufrago","Gabriel Garcia Marquez","Debolsillo",3000,"Ficcion");
-let libro2 = new Libro("Alicia en el pais de la maravillas","Carroll Lewis","Terramar",1000,"Lituratura juvenil");
-let libro3 = new Libro("El principito","Antoine De Saint Exupery","Ethos editorial",2000,"Infantil");
-
-let stockLibros = [libro1,libro2,libro3];
+let stockLibros = [libro1, libro2, libro3];
 
 console.log(libro1);
 console.log(libro2.getAutor());
